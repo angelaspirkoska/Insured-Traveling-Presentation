c603328f67a35957414f7620eb4ec138bd004cdb
Aleksandra <aleksandra.dimkovic@gmail.com>
2017-02-24 13:23:36 +0100

    read excel file and demo builder

---
+++ Authentication.WEB/Controllers/TestController.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using InsuredTraveling.DI;
+using System.Collections.Generic;
+using InsuredTraveling.Helpers;
+using InsuredTraveling.ViewModels;
+using InsuredTraveling.Filters;
+using AutoMapper;
+using System.IO;
+using System.Linq;
+using InsuredTraveling.ExcelReader;
+using OfficeOpenXml;
+
+namespace InsuredTraveling.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase excelConfigFile)
+        {
+
+            if (excelConfigFile != null && excelConfigFile.ContentLength > 0)
+            {
+                var path = @"~/ExcelConfig/config_file.xlsx";
+                path = System.Web.HttpContext.Current.Server.MapPath(path);
+                excelConfigFile.SaveAs(path);
+
+                ExcelFileViewModel e = new ExcelFileViewModel();
+                e.Path = path;
+
+                return View("PolicyForm",e);
+            }
+
+            return View();
+        }
+    }
+}
