9fad562e350f69dda2101846b2d128f346e2c579
atanasovski46 <atanasovski46@gmail.com>
2017-03-08 14:03:02 +0100

    Fixed Sign Up Validation Message
    Added Resources for Validation error at sign up view

--- Authentication.WEB/Models/User.cs
+++ Authentication.WEB/Models/User.cs
@@ -6,73 +6,73 @@ namespace InsuredTraveling.Models
 {
     public class User : IdentityUser
     {
-        [Required]
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         [Display(Name = "User_UserName" ,ResourceType = typeof(Resource))]
         public override string UserName { get; set; }
 
-        [Required]
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         [Display(Name = "User_FirstName" , ResourceType = typeof(Resource))]
         public string FirstName { get; set; }
 
-        [Required]
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         [Display(Name = "User_LastName" , ResourceType = typeof(Resource))]
         public string LastName { get; set; }
 
-        [Required]
-        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
+        [EmailAddress( ErrorMessageResourceType =typeof(Resource), ErrorMessageResourceName = "Error_Email")]
         [Display(Name = "User_Email", ResourceType = typeof(Resource))]
         public override string Email { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
+        [StringLength(100, ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Error_PasswordLen", MinimumLength = 6 )]
         [DataType(DataType.Password)]
         [Display(Name = "User_Password", ResourceType = typeof(Resource))]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "User_ConfirmPassword" , ResourceType = typeof(Resource))]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Compare("Password",ErrorMessageResourceType =typeof(Resource), ErrorMessageResourceName = "Error_PasswordNotMatch")]
         public string ConfirmPassword { get; set; }
 
-        [Required]
-        [RegularExpression("^[0-9]*$", ErrorMessage = "EMBG must be numeric")]
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
+        [RegularExpression("^[0-9]*$", ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Error_EMBG_Numeric")]
         [Display(Name = "User_EMBG", ResourceType = typeof(Resource))]
         public string EMBG { get; set; }
 
-        [Required]
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         [Display(Name = "User_Address", ResourceType = typeof(Resource))]
         public string Address { get; set; }
 
-        [Required]
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         [Display(Name = "User_Municipality",ResourceType =typeof(Resource))]
         public string Municipality { get; set; }
 
         [Display(Name = "User_InsuranceCompany", ResourceType = typeof(Resource))]
         public string InsuranceCompany = "Sava";
 
-        [RegularExpression("^[0-9]*$", ErrorMessage = "Phone number must be numeric")]
+        [RegularExpression("^[0-9]*$", ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Error_Phone_Numeric")]
         [Display(Name = "User_PhoneNumber", ResourceType = typeof(Resource))]
         public override string PhoneNumber { get; set; }
 
-        [Required]
-        [RegularExpression("^[0-9]*$", ErrorMessage = "Mobile number must be numeric")]
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
+        [RegularExpression("^[0-9]*$", ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Error_Mobile_Numeric")]
         [Display(Name = "User_MobilePhoneNumber", ResourceType = typeof(Resource))]
         public string MobilePhoneNumber { get; set; }
 
-        [Required]
-        [RegularExpression("^[0-9]*$", ErrorMessage = "Postal code must be numeric")]
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
+        [RegularExpression("^[0-9]*$", ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Error_PostalCode_Numeric")]
         [Display(Name = "User_PostalCode", ResourceType = typeof(Resource))]
         public string PostalCode { get; set; }
 
-        [Required]
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         [Display(Name = "User_PassportNumber", ResourceType = typeof(Resource))]
         public string PassportNumber { get; set; }
 
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         [Display(Name = "User_DateOfBirth", ResourceType = typeof(Resource))]
-        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DateOfBirth { get; set; }
 
-        [Required]
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         [Display(Name = "User_Gender", ResourceType = typeof(Resource))]
         public string Gender { get; set; }
 
