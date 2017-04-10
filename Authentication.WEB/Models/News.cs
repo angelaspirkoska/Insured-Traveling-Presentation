347d4fb3ea824669f8f46f7f7855f94ea774bbfe
atanasovski46 <atanasovski46@gmail.com>
2017-04-10 16:06:11 +0200

    Add news small fix

--- Authentication.WEB/Models/News.cs
+++ Authentication.WEB/Models/News.cs
@@ -1,12 +1,17 @@
-﻿namespace InsuredTraveling.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InsuredTraveling.Models
 {
     public class News
     {
         public string Id { get; set; }
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         public string Title { get; set; }
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         public string Content { get; set; }
+        
         public string InsuranceCompany { get; set; }
-
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         public string ImageLocation { get; set; }
     }
 }
