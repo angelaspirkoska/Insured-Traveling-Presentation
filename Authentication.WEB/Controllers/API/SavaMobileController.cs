df6e46b4694132773f4f94153af90c58a78ee654
SKostova <kostova.slobodanka@hotmail.com>
2017-04-17 16:20:49 +0200

    Api for saving sava policies

---
+++ Authentication.WEB/Controllers/API/SavaMobileController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace InsuredTraveling.Controllers.API
+{
+    [System.Web.Http.RoutePrefix("api/SavaMobile")]
+    public class SavaMobileController : ApiController
+    {
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("CreatePolicy")]
+        public IHttpActionResult CreatePolicy()
+        {
+
+            return Ok();
+        }
+    }
+}
