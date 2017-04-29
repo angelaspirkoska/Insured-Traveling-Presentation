8c5e253528fc390c6736b47cb1e17ba6304bc59a
SKostova <kostova.slobodanka@hotmail.com>
2017-04-29 15:10:10 +0200

    Bug fix - Show datepicker calendar in add event form

--- Authentication.WEB/Models/Event.cs
+++ Authentication.WEB/Models/Event.cs
@@ -10,28 +10,42 @@ namespace InsuredTraveling.Models
     public class Event
     {
         public int id { get; set; }
+
         public string CreatedBy { get; set; }
-        //public List<SearchRegisteredUser> ListUsers { get; set; }
+
         public int PeopleAttending { get; set; }
+
         [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         public string Description { get; set; }
+
         [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         public string Location { get; set; }
+
         [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         public string Organizer { get; set; }
+
         public string EventType { get; set; }
+
         [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         public string Title { get; set; }
+
         [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{00:dd/MM/yyyy}")]
         public DateTime StartDate { get; set; }
+
         [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{00:dd/MM/yyyy}")]
         public DateTime EndDate { get; set; }
+
         [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         public DateTime PublishDate { get; set; }
+
         [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         public TimeSpan StartTime { get; set; }
+
         [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         public TimeSpan EndTime { get; set; }
+
         public List<SelectListItem> EventTypes { get; set; }
         
     }
