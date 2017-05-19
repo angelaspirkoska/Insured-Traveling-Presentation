587141c7d4e4ed3fada83874b2380f15227515a6
atanasovski46 <atanasovski46@gmail.com>
2017-05-19 10:49:12 +0200

    Job shceduler added, still not implemented

---
+++ Authentication.WEB/Schedulers/EmailJob.cs
@@ -0,0 +1,27 @@
+﻿using Authentication.WEB.Services;
+using Quartz;
+using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace InsuredTraveling.Schedulers
+{
+    public class EmailJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            //TODO: Check if ticket with due date expired exists and send e-mail to the aasigned user
+            //var ticketName = "[TicketName]";
+            //var ticketNumber = "[TicketNumber]";
+            //var userName = "[TicketUser]";
+            //var ticketDate = "[TicketDate]";
+
+            var mailService = new MailService("atanasovski46@gmail.com");
+            var emailBody = "Dear Ivan now is "+ DateTime.Now;
+            mailService.setBodyText(emailBody);
+            mailService.setSubject("Shceduler");
+            mailService.sendMail();
+
+        }
+    }
+}
