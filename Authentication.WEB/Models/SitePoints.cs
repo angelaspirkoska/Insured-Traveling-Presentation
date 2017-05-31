11b79704dd49795308b9206c7ffab3703549253b
SKostova <kostova.slobodanka@hotmail.com>
2017-05-31 17:34:42 +0200

    SalePoints model (filled), controller and view added

---
+++ Authentication.WEB/Models/SitePoints.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace InsuredTraveling.Models
+{
+    public class SitePoints
+    {
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
+        public string LocationName { get; set; }
+
+        public bool insuranceDeal { get; set; }
+        public bool damageReport { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
+        public string Street { get; set; }
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
+        public string Municipality { get; set; }
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
+        public int PostalCode { get; set; }
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
+        public string PhoneNumber { get; set; }
+        public string OpeningHours { get; set; }
+
+
+    }
+}
