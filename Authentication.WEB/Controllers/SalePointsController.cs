56dc20a3e44a263f25fec4a846f990ed3685627f
SKostova <kostova.slobodanka@hotmail.com>
2017-06-06 12:00:49 +0200

    Changes in Sale Points - fields added

--- Authentication.WEB/Controllers/SalePointsController.cs
+++ Authentication.WEB/Controllers/SalePointsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using InsuredTraveling.Models;
 
 namespace InsuredTraveling.Controllers
 {
@@ -10,9 +11,8 @@ namespace InsuredTraveling.Controllers
     {
         // GET: SalePoints
         public ActionResult Index()
-        {
+        {    
             return View();
         }
-      
     }
 }
--- Authentication.WEB/Models/SalePoints.cs
+++ Authentication.WEB/Models/SalePoints.cs
@@ -5,22 +5,26 @@ namespace InsuredTraveling.Models
 {
     public class SalePoints
     {
+       
         [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         public string LocationName { get; set; }
 
-        public bool insuranceDeal { get; set; }
-        public bool damageReport { get; set; }
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
+        public string Municipality { get; set; }
 
         [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         public string Street { get; set; }
-        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
-        public string Municipality { get; set; }
+
         [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
         [RegularExpression("^[0-9]*$", ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "OnlyNumbers")]
         public int PostalCode { get; set; }
+
         [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "Required")]
-        public string PhoneNumber { get; set; }
+        
         public string OpeningHours { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool insuranceDeal { get; set; }
+        public bool damageReport { get; set; }
 
 
     }
