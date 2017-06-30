24bb1c46d190191084fb9713acd452e5df9f4c72
SKostova <kostova.slobodanka@hotmail.com>
2017-06-30 15:26:25 +0200

    String Helper added

---
+++ Authentication.WEB/Helpers/StringHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InsuredTraveling.Helpers
+{
+    public static class StringHelper
+    {
+        public static string Trunc(this string str, int maxLength)
+        {
+            return str.Substring(0, Math.Min(str.Length, maxLength)) +
+                (str.Length > maxLength ? " ... " : "");
+        }
+    }
+}
