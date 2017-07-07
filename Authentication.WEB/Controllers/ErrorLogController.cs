1868b3ac92b2bccdfad8232948f98c6c68b006d8
SKostova <kostova.slobodanka@hotmail.com>
2017-07-07 14:24:44 +0200

    GetErrorsXML deleted

--- Authentication.WEB/Controllers/ErrorLogController.cs
+++ Authentication.WEB/Controllers/ErrorLogController.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using InsuredTraveling.DI;
+using InsuredTraveling.Filters;
 
 namespace InsuredTraveling.Controllers
 {
+    [CustomActionFilter]
     public class ErrorLogController : Controller
     {
         // GET: ErrorLog
